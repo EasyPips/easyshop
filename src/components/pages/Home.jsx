3997706350ae2fce69f9f96aec0cd1f4021f0914
EasyPips <sundaybukunmi62@gmail.com>
2025-04-29 07:26:26 +0100

    comit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,15 @@
 import Banner from "../common/Banner";
 import ProductLists from "./ProductList";
 
-
 const Home = () => {
-    return ( 
-        <div className="pt-25 p-30 mx-auto text-center">
-            <div className="banner">
-                <Banner/>
-            </div>
-            <ProductLists/>
-        </div>
-     );
-}
- 
+  return ( 
+    <div className="pt-12 px-4 sm:px-8 lg:px-16 mx-auto text-center">
+      <div className="banner mb-8">
+        <Banner />
+      </div>
+      <ProductLists />
+    </div>
+  );
+};
+
 export default Home;
